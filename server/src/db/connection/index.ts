47158097ec24c408fada91f92042349b69985724
Neil Berg <neil.berg14@gmail.com>
2020-08-18 23:16:42 -0700

    server: env-based mongo URI

--- server/src/db/connection/index.ts
+++ server/src/db/connection/index.ts
@@ -6,7 +6,12 @@ import * as mongoose from 'mongoose';
  */
 export const connectToDB = async (): Promise<any> => {
   let disconnected = true;
-  const url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongo:27017/${process.env.MONGO_DATABASE}?authSource=admin`;
+  let url = '';
+  if (process.env.NODE_ENV === 'development') {
+    url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongo:27017/${process.env.MONGO_DATABASE}?authSource=admin`;
+  } else {
+    url = process.env.MONGODB_URI;
+  }
   while (disconnected) {
     try {
       await mongoose.connect(url, {
